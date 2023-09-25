8add13b6dc6b2092fec49d8f8d768aa2b439ad09
Michael// Deeft <MICBR157@GMAIL.COM>
2023-09-25 12:29:56 -0300

    primeiro commit

---
+++ Javascript/primeiros code/variaveis/variaveis.js
@@ -0,0 +1,16 @@
+//string
+var texto = "Curso de JavaScript";
+
+//number
+
+var numerointeiro = 1;
+var numerofracionado = 11.2;
+
+//boolean
+var testeboleano = true;
+
+//alert(numerointeiro);
+
+//document.write(texto);
+
+console.log(numerointeiro);
